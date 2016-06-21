6948502c421e507434d27549985d8a2c422ff0dd
Raphaël Gertz <git@rapsys.eu>
2016-06-21 13:14:27 +0200

    Fix user login generation Fix root.conf export format

--- lib/export.sh
+++ lib/export.sh
@@ -5,5 +5,5 @@ cat /dev/null > root.conf
 
 # Append every config parameters
 for i in `cat config/*.conf | perl -pne 'undef $_ if /^#/; s/=.*$//'`; do
-	eval echo ${i}=\'\$$i\' | tee -a root.conf
+	echo "$i='$(eval echo \$$i)'" | tee -a root.conf
 done
