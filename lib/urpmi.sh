8e2c032fc8f9550b165bc3f6584a43611a039302
Raphaël Gertz <git@rapsys.eu>
2023-11-15 13:08:11 +0100

    Ignore testing, debug and sources medias

--- lib/urpmi.sh
+++ lib/urpmi.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -e
 urpmi.removemedia -a
 urpmi.addmedia --distrib --all-media --mirrorlist http://mirrors.mageia.org/api/mageia.$MGARELEASE.$ARCH.list
-perl -pne 's%(.*testing.*) {\n%\1 {\n  ignore\n%i' -i /etc/urpmi/urpmi.cfg
+perl -pne 's%(.*(?:testing|debug|sources).*) {\n%\1 {\n  ignore\n%i' -i /etc/urpmi/urpmi.cfg
 #remove distrib-coffee line from mirrors cache
 #perl -pne 'undef $/; s% +{(?:\n +.(?:country|proximity|longitude|arch|version|zone|latitude).[^,]+,){7}\n +.url.[^,]+distrib-coffee[^,]+,(?:\n +.(?:type|proximity_corrected).[^,]+,){2}\n%%' -i /var/cache/urpmi/mirrors.cache
